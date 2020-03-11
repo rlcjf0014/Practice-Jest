f85d6e770236461e3930c93e15db247104dcff1e
rlcjf0014 <joshuarule2010@gmail.com>
2020-03-11 23:20:54 +0900

    Finally got es6 to work

--- README.md
+++
@@ -1,3 +0,0 @@
-# Practice-Jest
-
-In order to make test cases of my own, I need to practice Jest. 
--- test/sum.test.js
+++
@@ -1,5 +0,0 @@
-const sumTest = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sumTest(1, 2)).toBe(3);
-});
